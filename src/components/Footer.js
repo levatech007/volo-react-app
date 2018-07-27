06e18a312899d4a0e2775d44761a719b22a74667
Eva <evaliisagalen@gmail.com>
2018-07-27 14:05:11 +0300

    updated footer layout with social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,58 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Instagram from "../images/social_icons/instagram-logo.png";
+import Twitter from "../images/social_icons/twitter-logo.png";
+import Facebook from "../images/social_icons/facebook-logo.png";
 
 class Footer extends Component {
   render() {
     return(
-      <div className="container footer-background">
+      <div className="row justify-content-center footer-background">
+      <div className="container">
         <div className="row justify-content-center">
-          <div className="col-sm-3">
+          <div className="col-sm-2">
             <ul>
               <li>VOLO</li>
               <br></br>
-              <li>Locations</li>
-              <li>About</li>
-              <li>Log In</li>
-              <li>Sign Up</li>
+              <li><Link to={ '/locations' } >Locations</Link></li>
+              <li><Link to={ '/about' } >About</Link></li>
+              <li><Link to={ '/login' } >Log In</Link></li>
+              <li><Link to={ '/login' } >Sign Up</Link></li>
             </ul>
           </div>
-          <div className="col-sm-3">
-            <ul>
-              <li>CONNECT</li>
+          <div className="col-sm-2">
+              <p>CONNECT</p>
               <br></br>
-              <li>Facebook</li>
-              <li>Instagram</li>
-              <li>Twitter</li>
-            </ul>
+              <img src={ Instagram }></img>
+              <img src={ Twitter }></img>
+              <img src={ Facebook }></img>
           </div>
-          <div className="col-sm-3">
+          <div className="col-sm-4">
             <ul>
               <li>STAY TUNED</li>
               <br></br>
-              <li>Email field</li>
+              <form>
+                <div className="form-group">
+                  <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter your email ..."/>
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                  <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+              </form>
             </ul>
           </div>
           <div className="col-sm-3">
             <ul>
               <li>CONTACT</li>
               <br></br>
-              <li>Form</li>
-            
+              <li>Contact Us</li>
             </ul>
           </div>
         </div>
+        <div className="row justify-content-center">
+          <small>&copy; 2018 VOLO All rights reserved</small>
+        </div>
       </div>
+    </div>
     )
   }
 }
