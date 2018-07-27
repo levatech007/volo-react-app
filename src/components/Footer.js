5a8c35419f8ab030af07ee098039cab01b98659a
Eva <evaliisagalen@gmail.com>
2018-07-27 14:59:46 +0300

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,51 +8,59 @@ class Footer extends Component {
   render() {
     return(
       <div className="row justify-content-center footer-background">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-sm-2">
-            <ul>
-              <li>VOLO</li>
-              <br></br>
-              <li><Link to={ '/locations' } >Locations</Link></li>
-              <li><Link to={ '/about' } >About</Link></li>
-              <li><Link to={ '/login' } >Log In</Link></li>
-              <li><Link to={ '/login' } >Sign Up</Link></li>
-            </ul>
-          </div>
-          <div className="col-sm-2">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-sm-2">
+              <ul>
+                <li>VOLO</li>
+                <br></br>
+                <li><Link to={ '/locations' } className="footer-link" >Locations</Link></li>
+                <li><Link to={ '/about' } className="footer-link" >About</Link></li>
+                <li><Link to={ '/login' } className="footer-link" >Log In</Link></li>
+                <li><Link to={ '/login' } className="footer-link" >Sign Up</Link></li>
+              </ul>
+            </div>
+            <div className="col-sm-2">
               <p>CONNECT</p>
               <br></br>
-              <img src={ Instagram }></img>
-              <img src={ Twitter }></img>
-              <img src={ Facebook }></img>
-          </div>
-          <div className="col-sm-4">
-            <ul>
-              <li>STAY TUNED</li>
-              <br></br>
-              <form>
-                <div className="form-group">
-                  <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter your email ..."/>
-                  <button type="submit" className="btn btn-primary">Submit</button>
-                  <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              <div className="row">
+                <div className="col-3">
+                  <a href="..."><img src={ Instagram }></img></a>
                 </div>
-              </form>
-            </ul>
-          </div>
-          <div className="col-sm-3">
-            <ul>
-              <li>CONTACT</li>
+                <div className="col-3">
+                  <a href="..."><img src={ Twitter }></img></a>
+                </div>
+                <div className="col-3">
+                  <img src={ Facebook }></img>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <p>STAY TUNED</p>
               <br></br>
-              <li>Contact Us</li>
-            </ul>
+              <div className="row">
+                <div className="col-8">
+                  <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter your email ..."/>
+                </div>
+                <div className="col-4">
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+              </div>
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="col-sm-3">
+              <ul>
+                <li>CONTACT</li>
+                <br></br>
+                <li>Contact Us</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="row justify-content-center">
-          <small>&copy; 2018 VOLO All rights reserved</small>
+          <div className="row justify-content-center copyright">
+            <small>&copy; 2018 VOLO All rights reserved</small>
+          </div>
         </div>
       </div>
-    </div>
     )
   }
 }
