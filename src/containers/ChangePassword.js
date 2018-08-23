5a5c9446770df82c05e5269d96094723a2dfdf5b
Eva <evaliisagalen@gmail.com>
2018-08-23 13:05:28 +0300

    Created password reset form

--- src/containers/ChangePassword.js
+++ src/containers/ChangePassword.js
@@ -1,11 +1,93 @@
 import React, { Component } from "react";
+import Errors from "../components/Errors.js"
+import ReCAPTCHA from "react-google-recaptcha";
 
 class ChangePassword extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newPassword: "",
+      confirmPassword: "",
+      errors: "",
+      recaptchaResponse: "",
+    }
+    this.onNewPasswordInputChange = this.onNewPasswordInputChange.bind(this);
+    this.onConfirmPasswordInputChange = this.onConfirmPasswordInputChange.bind(this);
+    this.onNewPasswordSubmit = this.onNewPasswordSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onNewPasswordInputChange(e) {
+    this.setState({
+      newPassword: e.target.value,
+      confirmPassword: this.state.confirmPassword,
+    })
+  }
+
+  onConfirmPasswordInputChange(e) {
+    this.setState({
+      newPassword: this.state.newPassword,
+      confirmPassword: e.target.value,
+    })
+  }
+
+  onNewPasswordSubmit(e) {
+    e.preventDefault();
+    if (this.state.recaptchaResponse) {
+      console.log("OK!")
+    } else {
+      console.log("try again")
+    }
+  }
+
+  onChange(response) {
+    this.setState({
+    recaptchaResponse: response
+  });
+}
 
   render() {
     return (
-      <div>Change pw</div>
+      <div className="container">
+        { this.state.errors? <Errors errors={ this.state.errors } /> : null }
+        <div className="row align-items-center background">
+          <div className="row justify-content-md-center">
+            <form onSubmit={ this.onNewPasswordSubmit } className="forms">
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  onChange={ this.onNewPasswordInputChange }
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Confirm password"
+                  onChange={ this.onConfirmPasswordInputChange }
+                />
+              </div>
+              <div className="row justify-content-center">
+                <ReCAPTCHA
+                  ref="recaptcha"
+                  sitekey={ process.env.REACT_APP_SITE_KEY }
+                  onChange={ this.onChange }/>
+              </div>
+              <div className="row justify-content-md-center">
+                <input
+                  type="submit"
+                  className="btn btn-light"
+                  value="Submit"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     )
   }
+}
 
-  export default ChangePassword;
+export default ChangePassword;
