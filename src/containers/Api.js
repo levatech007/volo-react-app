77dda9fe3dcca0902cebc93b753ccf514e8b89ff
Eva <evaliisagalen@gmail.com>
2019-02-04 12:43:41 -0800

    added accordion and initial data layout from api documentation

--- src/containers/Api.js
+++ src/containers/Api.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { Accordion, AccordionItem, AccordionItemTitle, AccordionItemBody } from "react-accessible-accordion";
 
 class Api extends Component {
   constructor() {
     super();
     this.state = {
-      documentation: []
+      documentation: {}
     }
   }
 
@@ -12,21 +13,64 @@ class Api extends Component {
     fetch(`${process.env.REACT_APP_BACKEND_URL}/documentation`)
         .then((res) => {
           return res.json();
-        }).then((docs) => {
-          console.log(docs)
-        //   this.setState({ locations: locations })
-    });
+        }).then((json_data) => {
+          // console.log(documentation)
+          this.setState({ documentation: json_data.documentation })
+    })
   }
 
   render() {
     return(
       <div className="container">
-        <div className="row justify-content-center background">
-          <h3>VOLO API:</h3>
-          <div className="col-12">
-            
+        { this.state.documentation.current_routes &&
+          <div className="row justify-content-center background">
+            <h3>VOLO API:</h3>
+            <p>{ this.state.documentation.intro }</p>
+            <p>Base url for this api is { this.state.documentation.base_URL }</p>
+            <div className="col-12">
+                <Accordion>
+                  {
+                    this.state.documentation.current_routes.map((route, idx) => {
+                      return(
+                        <AccordionItem key={ idx }>
+                          <AccordionItemTitle>
+                            <div className="row">
+                              <div className="col-md-4"><h4>{ route.request_type }</h4></div>
+                              <div className="col-md-8"><p>{ route.route_url }</p></div>
+                            </div>
+                          </AccordionItemTitle>
+                          <AccordionItemBody>
+                            <p>{route.route_description}</p>
+                            <h5>Required parameters:</h5>
+                            <table>
+                              <tr>
+                                <th>Parameter</th>
+                                <th>Type</th>
+                                <th>Description</th>
+                              </tr>
+                              {
+                                route.required_params.map((param, idx) => {
+                                  return (
+                                    <tr>
+                                      <td>{ param.param }</td>
+                                      <td>{ param.type }</td>
+                                      <td>{ param.message }</td>
+                                    </tr>
+                                  )
+                                })
+                              }
+                            </table>
+                            <br></br>
+                            <p>{ route.notes }</p>
+                          </AccordionItemBody>
+                        </AccordionItem>
+                      )
+                    })
+                  }
+                </Accordion>
+            </div>
           </div>
-        </div>
+        }
       </div>
     )
   }
