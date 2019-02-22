68e899e8e2758fdf78a3f6d2ddbe91074aa51f64
Eva <evaliisagalen@gmail.com>
2019-02-21 17:06:59 -0800

    created a separate component for a dropdoen menu

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class DropdownMenu extends Component {
+
+  render() {
+    return(
+      <select onChange={ this.props.onchange } className="form-control form-control-lg">
+        <option defaultValue disabled>{ this.props.defaultValue }</option>
+          { this.props.dataArray.map((item, idx) => {
+            return(
+              <option value={item.id} key={idx}>{item.name}</option>
+              )
+            })
+          }
+        </select>
+    )
+  }
+}
+
+export default DropdownMenu;
