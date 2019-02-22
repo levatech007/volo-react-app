3b8e6e49bc50161dd2f62023617fe1928a2e4eae
Eva <evaliisagalen@gmail.com>
2019-02-21 17:06:26 -0800

    created component that displays aircraft info once one is selected

---
+++ src/components/AircraftInfo.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class AircraftInfo extends Component {
+  render() {
+    return(
+      <div className="row">
+        <div className="col-6">
+            <ul>
+              <li><p>Stats</p></li>
+              <li><p>will go</p></li>
+              <li><p>here</p></li>
+            </ul>
+          </div>
+        <div className="col-6">
+          Aircraft pic here
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AircraftInfo
