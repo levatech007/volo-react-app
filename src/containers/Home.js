177c1ef91529af6924ed49b675701d53eb170bf1
Eva <evaliisagalen@gmail.com>
2019-04-19 15:32:20 -0700

    added new info section to home page with icons

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
+import Logo     from "../images/logo.png"
+import HomeData from "../static-data/homepage.json";
 import "./styles/airplane-animations.css";
-import Logo from "../images/logo.png"
-import Airplane from "../images/table-icons/departure-icon.svg"
 
 class Home extends Component {
   render() {
     return (
+      <div>
         <div className="hero-animation">
           <img className="logo" src={ Logo } alt="logo"/>
-          <div class="flight run"></div>
-	           <div class="runway">
-	            </div>
+          <div className="flight run"></div>
+	           <div className="runway">
+	         </div>
         </div>
+
+        <div className="row justify-content-center info">
+          <div className="col-md-8">
+            <div className="row justify-content-center intro">
+                <p>VOLO is an app for avid planespotters. </p>
+            </div>
+            <div className="row">
+              {
+                HomeData.icons.map((icon, idx) => {
+                  return(
+                    <div className="col-md-4" key={ idx }>
+                      <img src={ require(`../images/homepage-icons/${ icon.name }-icon.svg`) } alt="airplane" />
+                      <p>{ icon.description }</p>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
