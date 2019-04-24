d2f54a05180572f82d264eb1c03329d98a858910
Eva <evaliisagalen@gmail.com>
2019-04-24 13:08:30 -0700

    added new Modal component with initial styling

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./modal.css";
+
+class Modal extends Component {
+
+  render() {
+    return(
+      <div className="screen">
+        <div className="modal-window">
+          MODAL
+        </div>
+      </div>
+    )
+  }
+}
+
+  export default Modal;
