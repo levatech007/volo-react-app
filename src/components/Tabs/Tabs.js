b04dcb1566c407a0c9ea0c3d22f2bf0c930cfd12
Eva <evaliisagalen@gmail.com>
2019-05-24 14:27:13 -0700

    created new tabs component

---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import './tabs.css'
+
+class Tabs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeTab: 0,
+    }
+    this.handleTabClick = this.handleTabClick.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  handleTabClick(tabIdx) {
+    this.setState({
+      activeTab: tabIdx
+    })
+    console.log(tabIdx)
+    this.props.handleTabsClick()
+  }
+
+  render() {
+    // for upto 3 tabs
+    return(
+      <div className="row justify-content-center">
+        {
+          this.props.tabs.map((tab, idx) => {
+            return(
+              <div
+                className={ this.state.activeTab === idx ? "col-4 tab tab-active" : "col-4 tab" }
+                onClick={ () => this.handleTabClick(idx) }
+                key={ idx }
+              >
+              { tab }
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Tabs
