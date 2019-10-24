5a69625c67f7bf57754e1c536a5eb71769fabdd8
Eva <evaliisagalen@gmail.com>
2019-10-23 18:54:57 -0700

    created new LocationReviewsPage to replace SingleLocation.js once completed

---
+++ src/containers/LocationReviewsPage/LocationReviewsPage.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import Auth                 from "j-toker";
+import $                    from "jquery";
+import LocationInfo         from "../../components/Location.js";
+import ReviewBanner         from "../../components/Banners/ReviewBanner.js";
+import LocationReviewForm   from "../../components/Forms/LocationReviewForm.js";
+import ReactStars           from "react-stars";
+import "./reviews-page.css"
+
+class ReviewsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+                    userId:               0,
+                    location:             {latitude: null, longitude: null},
+                    reviews:              [],
+                    reviewCount:          0,
+                    showForm:             false,
+                    latitude:             0,
+                    longitude:            0,
+                    errors:               "",
+                  }
+    this.showReviewForm         = this.showReviewForm.bind(this);
+    this.onSubmitReviewForm     = this.onSubmitReviewForm.bind(this);
+    this.renderLocationReviews  = this.renderLocationReviews.bind(this);
+  }
+
+  // componentWillMount() {
+  //   Auth.validateToken()
+  //   .then((user) => {
+  //     this.setState({ userId: user.id })
+  //   })
+  //   let locationId = this.props.match.params.id;
+  //   fetch(`${process.env.REACT_APP_BACKEND_URL}/locations/${locationId}.json`)
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then((location) => {
+  //     this.setState({
+  //                     location:     location,
+  //                     reviewCount:  location.reviews.length,
+  //                     reviews:      location.reviews.reverse(),
+  //                     latitude:     location.latitude,
+  //                     longitude:    location.longitude,
+  //                   })
+  //   });
+  //   // .fail(resp)
+  // }
+
+  showReviewForm(e) {
+    e.preventDefault();
+    this.setState({ showForm: true })
+  }
+
+  onSubmitReviewForm(review) {
+    let currentReviews = this.state.reviews
+    $.ajaxSetup({
+      beforeSend(xhr, settings) {
+        Auth.appendAuthHeaders(xhr, settings);
+      }
+    });
+    $.post({
+      url: `${process.env.REACT_APP_BACKEND_URL}/reviews`,
+      data: {
+              author:       review.author,
+              title:        review.title,
+              content:      review.content,
+              rating:       review.rating,
+              location_id:  this.state.location.id,
+            },
+      success: (data) => {
+        let newReview = [data.review]
+        this.setState({
+                        reviews:      newReview.concat(currentReviews),
+                        showForm:     false,
+                        reviewCount:  this.state.reviewCount + 1
+                      })
+      },
+    });
+    this.setState({ showForm: false})
+  }
+
+  renderLocationReviews() {
+    return(
+      <ReviewBanner />
+    )
+  }
+
+
+  render() {
+    let showReviewForm = this.state.showForm
+    return(
+      <div className="container">
+        <div className="row background">
+          <div className="col-md-12">
+            { this.state.location.latitude && <LocationInfo location={ this.state.location } reviewCount={ this.state.reviewCount}/> }
+            { showReviewForm ?
+              <LocationReviewForm
+                onSubmitReviewForm={ this.onSubmitReviewForm }
+                locationId={ this.state.location.id }
+              />
+              :
+              this.renderLocationReviews()
+            }
+            { this.state.userId &&
+              <div className="row justify-content-center">
+                <button onClick={ this.showReviewForm } className="footer-btn submit button-margin">Add review</button>
+              </div>
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsPage
